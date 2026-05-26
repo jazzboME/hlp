f3a9ccb81bd6ee3aad6d464b50e4a20e2ca0c5fe
Kevin Foss <kevin.foss@gmail.com>
2026-05-25 21:55:44 -0400

    Switch to personal fork Add Crypto

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/jazzboME/hlp
 go 1.25.0
 
 require (
-	github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1 // indirect
-	github.com/spf13/pflag v1.0.10 // indirect
-	github.com/the-trader-dev/tiin-go v1.0.0 // indirect
+	github.com/jazzboME/tiin-go v0.0.0-20260526015435-044f420ef8ef
+	github.com/spf13/pflag v1.0.10
 )
+
+require github.com/gocarina/gocsv v0.0.0-20260523204920-c264028e67ea // indirect
--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 	"time"
 
 	flag "github.com/spf13/pflag"
-	tiingo "github.com/the-trader-dev/tiin-go"
+	tiingo "github.com/jazzboME/tiin-go"
 )
+
+var markets = []string{ "c", "y", "s" }
 
 func main() {
 	var today = time.Now().Truncate(24 * time.Hour)
 	var isoFormat = "2006-01-02"
 
 	// command line flags
+	var market = flag.StringP("market", "m", "s", "name of market to query, s (security), c (currency) or y (crypto)")
 	var ticker = flag.StringP("ticker", "t", "", "name of ticker to pull values for")
 	var startDate = flag.TimeP("start", "s", today, []string{isoFormat}, "first date to pull prices for, in YYYY-MM-DD")
 	var endDate = flag.TimeP("end", "e", today, []string{isoFormat}, "last day to pull prices for, in YYYY-MM-DD")
@@ -25,8 +29,11 @@ func main() {
 		fail("No ticker supplied", nil)
 	}
 
+	if !slices.Contains(markets, *market) {
+		fail(fmt.Sprintf("Market must be in %v", markets), nil)
+	}
  	// internal variables
-	var pr []tiingo.EodPrice
+	//var pr []tiingo.EodPrice
 	ctx := context.Background()
 	// get Tiingo Token
 	token := os.Getenv("TIINGO_TOKEN")
@@ -36,6 +43,59 @@ func main() {
 	}
 
 	c := tiingo.NewClient(token)
+
+	switch *market {
+	case "s":
+		//var pr []tiingo.EodPrice
+		columns := []string{"date", "adjClose", "divCash", "splitFactor"}
+		var e = tiingo.EodPriceParams{
+			StartDate: *startDate,
+			EndDate: *endDate,
+			ResampleFreq: tiingo.Daily,
+			Sort: tiingo.DateAsc,
+			RespFormat: tiingo.JSON,
+			Columns: columns,
+		}
+		fmt.Println(e)
+		resp, err := c.EodPrice(ctx, *ticker, &e)
+		if err != nil {
+			fail("failed to get security prices", err)
+		}
+		for _, price := range resp {
+		fmt.Println(price)
+		var cmt string
+		if price.DivCash > 0 { cmt += fmt.Sprintf(" Ex-dividend of %f.", price.DivCash) }
+		if price.SplitFactor != 1.0 { cmt += fmt.Sprintf(" Split at %.2f ratio.", price.SplitFactor) }
+		if len(cmt) > 0 {
+			cmt = " ;" + cmt
+		}
+		fmt.Printf("P %s %s %.8f USD%s\n", price.Date.Format(isoFormat), *ticker, price.AdjClose, cmt)
+		}
+	case "y":
+		var e = tiingo.CryptoPriceParams{
+			Exchanges: []string{"POLONIEX","GDAX"},
+			StartDate: *startDate,
+			EndDate: *endDate,
+			ResampleFreq: tiingo.OneDay,
+		}
+		resp, err := c.CryptoPrice(ctx, []string{*ticker}, &e)
+		if err != nil {
+			fail("failed to get crypto prices", err)
+		}
+		for _, ticker := range resp {
+			for _, price := range ticker.PriceData {
+				fmt.Printf("P %s %s %.8f %s\n", price.Date.Format(isoFormat), strings.ToUpper(ticker.BaseCurrency), 
+							price.Close, strings.ToUpper(ticker.QuoteCurrency))
+
+			}
+		}
+	}
+		//err = json.Unmarshal(resp, &pr)
+		//if err != nil {
+		//	fail("failed to unmarshal securities, err")
+		//}
+	
+	/*
 	columns := []string{"date", "adjClose", "divCash", "splitFactor"}
 	resp, err := c.EodPrice(ctx, *ticker, *startDate, *endDate, tiingo.Daily, tiingo.DateAsc, tiingo.JSON, columns)
 	if err != nil {
@@ -46,8 +106,9 @@ func main() {
 	if err != nil {
 		fail("failed to unmarshal results", err)
 	}
-
-	for _, price := range pr {
+	*/
+/*
+	for _, price := range resp {
 		fmt.Println(price)
 		var cmt string
 		if price.DivCash > 0 { cmt += fmt.Sprintf(" Ex-dividend of %f.", price.DivCash) }
@@ -57,6 +118,7 @@ func main() {
 		}
 		fmt.Printf("P %s %s %.8f USD%s\n", price.Date.Format(isoFormat), *ticker, price.AdjClose, cmt)
 	}
+*/
 }
 
 func fail(reason string, err error) {
